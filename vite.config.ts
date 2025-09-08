c909d14f72828a410176ce40217612b61a4e597b
Sumit Jain <sum.jain10@gmail.com>
2025-09-08 16:36:05 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   server: {
-    allowedHosts: ['my-sandbox-app.fly.dev'],
+    allowedHosts: ['my-sandbox-app.fly.dev','vite-test-app-1.fly.dev'],
   }
 });
